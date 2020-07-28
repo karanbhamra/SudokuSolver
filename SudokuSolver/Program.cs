2cc77c265eb50cb5a85a81da12f8942a1c57e161
Karan Bhamra <karan.bhamra@gmail.com>
2020-07-27 20:02:57 -0700

    Added ability to solve, can also generate if some are left blank. Also ability to print as 3x3 squares with time taken to solve it.

---
+++ SudokuSolver/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Diagnostics;
+
+namespace SudokuSolver
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var board = LoadFileToArray("input.txt");
+            var solver = new SudokuSolver(board);
+
+            var stopwatch = Stopwatch.StartNew();
+            
+            var result = solver.Solve();
+            
+            stopwatch.Stop();
+
+            var timeTaken = stopwatch.ElapsedMilliseconds;
+            
+            if (result)
+            {
+                solver.PrintBoard();
+                Console.WriteLine($"Took {timeTaken}ms.");
+            }
+            else
+            {
+                Console.WriteLine("Cannot solve.");
+            }
+        }
+
+        private static int[][] LoadFileToArray(string filename)
+        {
+            const int size = 9;
+            var board = new int[size][];
+
+            var fileLines = File.ReadAllLines(filename);
+
+            var row = 0;
+            foreach (var line in fileLines)
+            {
+                board[row] = line.Select(x => x - '0').ToArray();
+                row++;
+            }
+
+            return board;
+        }
+    }
+}
---
+++ SudokuSolver/SudokuSolver.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace SudokuSolver
+{
+    // 800000000
+    // 003600000
+    // 070090200
+    // 050007000
+    // 000045700
+    // 000100030
+    // 001000068
+    // 008500010
+    // 090000400
+    public class SudokuSolver
+    {
+        private readonly int[][] _board;
+        private const int Size = 9;
+
+        public SudokuSolver(int[][] board)
+        {
+            _board = board;
+        }
+
+        public void PrintBoard()
+        {
+            for (var row = 0; row < Size; row++)
+            {
+                for (var col = 0; col < Size; col++)
+                {
+                    var ch = ' ';
+                    if ((col + 1) % 3 == 0 && col > 0)
+                    {
+                        ch = '\t';
+                    }
+
+                    Console.Write($"{_board[row][col]}{ch}");
+                }
+
+                if ((row + 1) % 3 == 0 && row > 0 && row < Size - 1)
+                {
+                    Console.WriteLine();
+                }
+
+                if (row < Size)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        public bool Solve()
+        {
+            for (int row = 0; row < Size; row++)
+            {
+                for (int col = 0; col < Size; col++)
+                {
+                    if (_board[row][col] == 0) // empty
+                    {
+                        for (int guess = 1; guess <= 9; guess++)
+                        {
+                            if (IsValid(row, col, guess))
+                            {
+                                _board[row][col] = guess;
+
+                                if (Solve())
+                                {
+                                    return true;
+                                }
+                                else
+                                {
+                                    _board[row][col] = 0;
+                                }
+                            }
+                        }
+
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private bool IsValid(int row, int col, int guess)
+        {
+            return IsRowValid(row, guess) &&
+                   IsColValid(col, guess) &&
+                   IsSquareValid(row, col, guess);
+        }
+
+        private bool IsRowValid(int row, int guess)
+        {
+            for (var col = 0; col < Size; col++)
+            {
+                if (_board[row][col] == guess)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool IsColValid(int col, int guess)
+        {
+            for (var row = 0; row < Size; row++)
+            {
+                if (_board[row][col] == guess)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool IsSquareValid(int row, int col, int guess)
+        {
+            var r = row - row % 3;
+            var c = col - col % 3;
+
+            for (var i = r; i < r + 3; i++)
+            {
+                for (var j = c; j < c + 3; j++)
+                {
+                    if (_board[i][j] == guess)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
